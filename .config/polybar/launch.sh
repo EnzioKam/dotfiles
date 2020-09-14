411f75b6c0be7752b26c36c07a1dac2a645c15ed
EnzioKam <enziokamhh@gmail.com>
2020-09-14 15:34:13 +0800

    Update multi-monitor setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,18 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
+outputs=$(polybar --list-monitors | cut -d ":" -f1)
+lines=$(polybar --list-monitors | cut -d ":" -f1 | wc -l)
+
+if [[ $lines == 2 ]]; then
+    for m in $outputs; do
+        if [[ $m == "HDMI-1-1" ]]; then
+            polybar -c ~/.config/polybar/config-pri.ini primary &
+        elif [[ $m == "eDP1" ]]; then
+            polybar -c ~/.config/polybar/config-sec.ini secondary &
+        fi
+    done
+else
     MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
-done
+fi
+
