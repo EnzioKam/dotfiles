cd7323edb425efe3b252e74f021c91a1d053ae70
EnzioKam <enziokamhh@gmail.com>
2021-08-04 16:44:04 +0800

    Update neovim configuration to lua

---
+++ dotfiles/config/nvim/init.lua
@@ -0,0 +1,83 @@
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd("pwd")
+local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+local opt = vim.opt  -- to set options
+
+require "paq" {
+    "savq/paq-nvim"; -- Let Paq manage itself
+    "junegunn/fzf.vim";
+    "nvim-lua/plenary.nvim";
+    "lewis6991/gitsigns.nvim";
+    "lukas-reineke/indent-blankline.nvim";
+    "dstein64/nvim-scrollview";
+    "shaunsingh/nord.nvim";
+    "nvim-treesitter/nvim-treesitter";
+    "p00f/nvim-ts-rainbow";
+    "terrortylor/nvim-comment";
+    "mboughaba/i3config.vim";
+    "ap/vim-css-color";
+    "kyazdani42/nvim-web-devicons";
+    "kyazdani42/nvim-tree.lua";
+    "hoob3rt/lualine.nvim";
+}
+require("gitsigns").setup()
+require("indent_blankline").setup {
+    char = "|",
+    buftype_exclude = {"terminal"}
+}
+require("nvim-treesitter.configs").setup({
+    ensure_installed = "maintained",
+    hightlight = {enable = true},
+    rainbow = {enable = true, extended_mode = true}
+})
+require("nvim_comment").setup({
+    line_mapping = ";;",
+    operator_mapping = ";"
+})
+require("lualine").setup({
+    options = {theme = "nord"}
+})
+require("bindings")
+
+g.colors_name = "nord"
+
+opt.completeopt = {"menuone", "noinsert", "noselect"}
+cmd "syntax on"
+cmd "filetype plugin indent on"
+opt.timeoutlen = 500
+opt.autoindent = true
+opt.autoread = true
+opt.backspace = {"indent", "eol", "start"}
+opt.bs = "2"
+opt.display = "lastline"
+opt.hlsearch = true
+opt.incsearch = true
+opt.mouse = "a"
+opt.smarttab = true
+opt.softtabstop = 4
+opt.showcmd = true
+opt.showmode = true
+opt.showmatch = true
+opt.report = 0
+opt.linespace = 5
+opt.pumheight = 20
+opt.number = true
+opt.expandtab = true
+opt.hidden = true
+opt.ignorecase = true
+opt.joinspaces = false
+opt.list = true
+opt.number = true
+opt.scrolloff = 4
+opt.shiftround = true
+opt.shiftwidth = 4
+opt.sidescrolloff = 8
+opt.smartcase = true
+opt.smartindent = true
+opt.splitright = true
+opt.splitbelow = true
+opt.tabstop = 4
+opt.termguicolors = true
+opt.wildmode = {"list", "longest"}
+opt.wrap = false
+
---
+++ dotfiles/config/nvim/lua/bindings.lua
@@ -0,0 +1,85 @@
+local g = vim.g
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend("force", options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+g.mapleader = " "
+map("n", "<leader>b", ":Buffers<CR>")
+map("n", "<leader>h", ":History<CR>")
+map("n", "<leader>f", ":Files<CR>")
+map("n", "<C-n>", ":NvimTreeToggle<CR>")
+
+-- Navigate windows
+map("n", "<C-h>", "<C-w>h", {noremap = true})
+map("n", "<C-j>", "<C-w>j", {noremap = true})
+map("n", "<C-k>", "<C-w>k", {noremap = true})
+map("n", "<C-l>", "<C-w>l", {noremap = true})
+
+-- Original remappings from vim-better-default at
+-- https://github.com/liuchengxu/vim-better-default
+map("n", "<leader>q", ":q<CR>", {noremap = true})
+map("n", "<leader>Q", ":qa!<CR>", {noremap = true})
+-- Move half page faster
+map("n", "<leader>d", "<C-d>", {noremap = true})
+map("n", "<leader>u", "<C-u>", {noremap = true})
+-- Bash like
+map("i", "<C-a>", "<Home>", {noremap = true})
+map("i", "<C-e>", "<End>", {noremap = true})
+map("i", "<C-d>", "<Delete>", {noremap = true})
+map("n", "<C-a>", "<Home>", {noremap = true})
+map("n", "<C-e>", "<End>", {noremap = true})
+map("n", "<C-d>", "<Delete>", {noremap = true})
+-- jj | escaping
+map("i", "jj", "<Esc>", {noremap = true})
+map("c", "jj", "<C-c>", {noremap = true})
+-- Quit visual mode
+map("v", "v", "<Esc>", {noremap = true})
+-- Move to start/end of line
+map("n", "H", "^", {noremap = true})
+map("n", "L", "$", {noremap = true})
+-- Redo
+map("n", "U", "<C-r>", {noremap = true})
+-- Yank to end of line
+map("n", "Y", "y$", {noremap = true})
+-- Open shell in nvim
+map('n', "<leader>t", ":terminal<CR>")
+
+-- Buffers
+map("n", "<leader>bp", ":bprevious<CR>", {noremap = true})
+map("n", "<leader>bn", ":bnext<CR>", {noremap = true})
+map("n", "<leader>bf", ":bfirst<CR>", {noremap = true})
+map("n", "<leader>bl", ":blast<CR>", {noremap = true})
+map("n", "<leader>bd", ":bd<CR>", {noremap = true})
+map("n", "<leader>bk", ":bw<CR>", {noremap = true})
+-- File save
+map("n", "<leader>fs", ":update<CR>", {noremap = true})
+-- Moving and navigating windows
+map("n", "<leader>ww", "<C-W>w", {noremap = true})
+map("n", "<leader>wr", "<C-W>r", {noremap = true})
+map("n", "<leader>wd", "<C-W>c", {noremap = true})
+map("n", "<leader>wn", ":vnew<CR>", {noremap = true})
+map("n", "<leader>wm", ":new<CR>", {noremap = true})
+map("n", "<leader>wq", "<C-W>q", {noremap = true})
+map("n", "<leader>wh", "<C-W>h", {noremap = true})
+map("n", "<leader>wj", "<C-W>j", {noremap = true})
+map("n", "<leader>wk", "<C-W>k", {noremap = true})
+map("n", "<leader>wl", "<C-W>l", {noremap = true})
+map("t", "<leader>wh", "<C-W>h", {noremap = true})
+map("t", "<leader>wj", "<C-W>j", {noremap = true})
+map("t", "<leader>wk", "<C-W>k", {noremap = true})
+map("t", "<leader>wl", "<C-W>l", {noremap = true})
+-- Window sizing
+map("n", "<leader>wH", "<C-W>5<", {noremap = true})
+map("n", "<leader>wL", "<C-W>5>", {noremap = true})
+map("n", "<leader>wJ", ":resize +5<CR>", {noremap = true})
+map("n", "<leader>wK", ":resize -5<CR>", {noremap = true})
+map("n", "<leader>w=", "<C-W>=", {noremap = true})
+-- Duplicating windows
+map("n", "<leader>ws", "<C-W>s", {noremap = true})
+map("n", "<leader>w-", "<C-W>s", {noremap = true})
+map("n", "<leader>wv", "<C-W>v", {noremap = true})
+map("n", "<leader>w2", "<C-W>v", {noremap = true})
+
